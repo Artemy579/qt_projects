3f404cddb12023e9b24511af688d689877a30eb6
Artemy579 <82162672+Artemy579@users.noreply.github.com>
2021-09-05 17:43:09 +0300

    Add files via upload

---
+++ src/mainwindow.h
@@ -0,0 +1,39 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QMessageBox>
+#include <QDebug>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+signals:
+    void next_attempt();
+private slots:
+    void pushButton_clicked();
+    void try_to_guess();
+private:
+    Ui::MainWindow *ui;
+    int a = 0;
+    int b = 2000;
+    int c = 0;
+    QMessageBox *msg_box;
+    QPushButton *pb_less;
+    QPushButton *pb_greater;
+    QPushButton *pb_thats_it;
+    QPushButton *pb_terminate;
+
+    void create_msg_box();
+
+};
+#endif // MAINWINDOW_H
