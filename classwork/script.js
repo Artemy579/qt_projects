b959c032d245341aaa5454f9776d31b91b1d15a1
Artemy579 <82162672+Artemy579@users.noreply.github.com>
2022-03-27 17:53:54 +0300

    Add files via upload

---
+++ classwork/script.js
@@ -0,0 +1,81 @@
+ /*
+ let array = []
+ for(let i = 0; i < 10; ++i){
+    array.push(i)
+    console.log(array[i])
+ }
+
+
+ let arr = []
+ for(let i = 0; i < 8; ++i){
+     arr.push([])
+     for(let j = 0; j < 8; ++j){
+        arr[i][j]= (i+j) % 3    
+     }
+ }
+console.log(arr)
+*/
+/*
+let array = []
+ 
+   array.push("fhgfh","fdgfd","ttrye") 
+console.log(array)
+
+const array2 = array
+.reduce((acc, value) => {  
+    value.split(',')
+    //  acc.push(value)
+    return acc
+})
+
+//.filter(value => value > 50)
+//.map(value => value ** 2)
+/*.reduce((acc, value) => { 
+    acc.push(value **2)
+    return acc
+}, [])
+*/
+/* 
+function print_array2(array){
+    array.forEach((value,i,array) =>
+    {
+        //console.log(value)
+        console.log("Эелемент ", i, "из 14 (пройдено:", Math.floor((i/14)*100),"% ", value)
+    })
+}
+//print_array2(array)
+console.log(array2)
+
+function create_vagons(){
+    
+}
+
+
+function get_train_info(vagons){
+    return{
+        count: 10,
+        types:{
+            cargo: 500,
+            liquide: 250,
+        }
+    }
+}
+
+console.log(vagons)
+*/
+
+function train(){
+    return{
+        types:{
+            cargo,
+            passenger,
+        }
+    }
+}
+
+function route(){
+    return{
+        name,
+        stations[]
+    }
+}
